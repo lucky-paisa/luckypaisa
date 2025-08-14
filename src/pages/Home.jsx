5464d6e4873417b68cb60fd5e81142ab9864e29d
Hammad Iqbal Khan <hammadiqbalk2000@gmail.com>
2025-08-15 00:41:32 +0500

    Fix Home.css import path case

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import './styles/home.css';
+import './styles/Home.css';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState, useRef } from 'react';  
