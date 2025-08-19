dc6a091820d56c39b43e1020f5ae2343b67dcd25
Hammad Iqbal Khan <hammadiqbalk2000@gmail.com>
2025-08-19 18:02:55 +0500

    updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -302,7 +302,7 @@ const unsubscribePurchases = onSnapshot(purchasesRef, async (querySnapshot) => {
 
 
 useEffect(() => {
-  const messages = ['Invest ✓', 'Earn ✓', 'Reward ✓'];
+  const messages = ['Invest', 'Earn', 'Reward'];
   let index = 0;
   let charIndex = 0;
   let currentText = '';
@@ -1022,7 +1022,7 @@ const formatPlanName = (planId) => {
       <br/>
       <br/>
       <h2 className="animated-heading">
-         Buy our exclusive plans <br/> <span className="typing">{animatedText}</span></h2>
+         Buy our exclusive plans <br/> <span className="typing">✓ {animatedText}</span></h2>
       <br/> 
       <div className="sliderContainer">
         {plans.map((plan) => (
@@ -1230,7 +1230,6 @@ const formatPlanName = (planId) => {
             </div>
             <div className="profile-row">
               <span>💳 Wallet Address</span>
-              <br/>
               <span style={{ wordBreak: "break-word", maxWidth: "200px", textAlign: "right" }}>
                 {userData?.walletAddress || "N/A"}
               </span>
